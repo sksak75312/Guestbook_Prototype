cb1a66a6ba4601a5795e582ff7468d4b9b56c30b
Eero <sksak75312@gmail.com>
2025-08-11 16:59:24 +0800

    feat: 新增 index 路由

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express from 'express';
+import indexRoute from './routes';
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (_, req) => {
-  req.json({
-    name: 'Express Response',
-  });
-});
+app.use('/', indexRoute);
 
 app.listen(PORT);
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (_, res) => {
+  res.send('<h1 style="width: 100%; margin: 0; text-align: center;">Guestbook</h1>')
+})
+
+export default router;
