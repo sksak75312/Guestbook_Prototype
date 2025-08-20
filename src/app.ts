c57a80b04a4cd1f8290ddb748caa94a9fc9f08ef
Eero <sksak75312@gmail.com>
2025-08-20 14:54:14 +0800

    feat: cors 暫時允許所有存取

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
-import 'dotenv/config'; // 需要提早引入，後續的檔案才可以正常使用到 dotenv
+// 需要提早引入，後續的檔案才可以正常使用到 dotenv
+import 'dotenv/config';
+import cors from 'cors';
 import indexRoute from './routes';
 import guestbookRoute from './routes/guestbookRoute';
 
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // 需要增加這一段解析 JSON 格式，post 才可取用到 req.body 資料
 app.use(express.json());
+app.use(cors())
 
 app.use('/', indexRoute);
 app.use('/guestbook', guestbookRoute);
