8e42e5fe344327c861ffee30fcb4e6f1573d2c4d
Eero <sksak75312@gmail.com>
2025-10-30 17:53:08 +0800

    types(enum): 新增 enum success message

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import * as commentsService from '../services/comments.service';
 
+import { SuccessMessage } from '../types/message.enum';
+
 export async function getSingleProject(
   req: Request,
   res: Response,
@@ -29,7 +31,7 @@ export async function postSingleProjectMessage(
     await commentsService.postSingleProjectMessage(projectId, userId, message);
     res.status(201).json({
       status: 201,
-      message: '新增留言成功',
+      message: SuccessMessage.POST_COMMENT_SUCCESS,
     });
   } catch (error) {
     next(error);
@@ -62,7 +64,7 @@ export async function postNewProject(
     await commentsService.postNewProject(project);
     res.status(201).json({
       status: 201,
-      message: `資料庫創建成功`,
+      message: SuccessMessage.POST_PROJECT_SUCCESS,
     });
   } catch (error) {
     next(error);
@@ -79,7 +81,7 @@ export async function deleteSingleProjectMessage(
     await commentsService.deleteSingleProjectMessage(projectId, messageId);
     res.status(200).json({
       status: 200,
-      message: '成功刪除指定留言',
+      message: SuccessMessage.DELETE_COMMENT_SUCCESS,
     });
   } catch (error) {
     next(error);
--- src/types/message.enum.ts
+++ src/types/message.enum.ts
@@ -1,3 +1,9 @@
+export enum SuccessMessage {
+  POST_PROJECT_SUCCESS = '新專案創建成功',
+  POST_COMMENT_SUCCESS = '新增留言成功',
+  DELETE_COMMENT_SUCCESS = '成功刪除留言',
+}
+
 export enum ErrorMessage {
   SEARCH_NOT_PROJECT = '查無此專案，請重新確認專案名稱',
   POST_COMMENT_FAILURE = '留言失敗!，請確認專案相關資訊',
